a2dd69e77c998fac731b1e4cf3db972cbf29b422
Kazuho Ishida <k10700110@gmail.com>
2023-02-01 20:52:50 +0900

    move js files from assets to js dir

--- assets/password-modal.js
+++ assets/password-modal.js
@@ -1,9 +1,9 @@
-class PasswordModal extends DetailsModal {
+// src/js/password-modal.js
+var PasswordModal = class extends DetailsModal {
   constructor() {
     super();
-
-    if (this.querySelector('input[aria-invalid="true"]')) this.open({target: this.querySelector('details')});
+    if (this.querySelector('input[aria-invalid="true"]'))
+      this.open({ target: this.querySelector("details") });
   }
-}
-
-customElements.define('password-modal', PasswordModal);
+};
+customElements.define("password-modal", PasswordModal);
